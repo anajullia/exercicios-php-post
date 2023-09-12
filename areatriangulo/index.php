e9820498b6915de71d8ae3554dbefcd69269cfb5
Ana Jullia <105755016+anajullia@users.noreply.github.com>
2023-09-12 09:48:00 -0300

    Triangulo

---
+++ areatriangulo/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Área Triângulo</title>
+</head>
+<body>
+    <form action="resultadotriangulo.php" method="post">
+
+        <p>Bem-Vindo(a) a calculadora de área!</p>
+
+        Base: <input type="text" name="base"><br>
+        Altura: <input type="text" name="altura"><br>
+        <br>
+        <input type="submit">
+    </form>
+</body>
+</html>
---
+++ areatriangulo/resultadotriangulo.php
@@ -0,0 +1,9 @@
+<?php
+
+$base = ($_POST['base']);
+$altura = ($_POST['altura']);
+$area = ($base * $altura) / 2;
+
+
+echo "A área do triângulo, em centímetros, é: $area";
+?>
